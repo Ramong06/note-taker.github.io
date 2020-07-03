30b2b5195291e14a72c55b71d08f45132b22a633
Ramon Garza III <59939116+Ramong06@users.noreply.github.com>
2020-07-02 23:22:24 -0500

    server listening on port 8080

--- public/assets/server.js
+++ public/assets/server.js
@@ -1,10 +1,43 @@
-var express = require('express');
-var app = express();
+// Dependencies
+// ===========================================================
 
+var express = require('express');
+var path = require("path");
+var app = express();
 var PORT = process.env.PORT || 8080;
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
 
-// require("./routes/...")(app);
-// require("./routes/...")(app);
+// YOUR CODE GOES HERE
+
+//
+
+// Routes
+// ===========================================================
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// app.get('/yoda', function (req, res) {
+//     res.json(yoda);
+// });
+
+// app.get('/darthmaul', function (req, res) {
+//     res.json(darthmaul);
+// });
+
+// Create a new Express route that leads users to the new Obi Wan Kenobi Data
+// Follow the same format as the Yoda and Darth Maul routes
+//
+
+// YOUR CODE GOES HERE
+//
+//
+// app.get('/obiwankenobi', function (request, response) {
+//     response.json(obiwankenobi);
+// });
+
+// Listener
+// ===========================================================
+app.listen(PORT, function () {
+    console.log('App listening on PORT ' + PORT);
+});
